a8bac15b59d364a77a62fb981e48fa60fde0e16b
Aaron Stack <aaron.stack@area23hc.com>
2022-01-21 13:13:52 -0500

    - only run on zoom launch page

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,21 @@
 /************* CONSTANTS *************/
 
-const name;
+const name = "";
 
 /*************** MAIN ****************/
 window.addEventListener("load", function (event) {
+  const container = document.getElementsByClassName("zI5ZBcpf")[0];
+  if (!container) {
+    return;
+  }
+
   const div = document.createElement("div");
   div.innerHTML = `<a id="findMe" web_client="" tabindex="0">Join from Your Browser</a>`;
-  document.getElementsByClassName("zI5ZBcpf")[0].appendChild(div);
+  container.appendChild(div);
   document.getElementById("findMe").click();
 
   const nameInput = document.getElementById("inputname");
   if (!nameInput.value && name) {
-    nameInput.value = config.name;
+    nameInput.value = name;
   }
 });
